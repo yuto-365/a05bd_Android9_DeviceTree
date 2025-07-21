11cbd810a9578de7469d426c06d21872f0398c8b
yuto-365 <yyttoo089@hotmail.com>
2025-07-21 12:40:12 +0900

    ファイルを追加

---
+++ Android.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),TAB-A05-BD)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
---
+++ README.md
@@ -0,0 +1,9 @@
+# Android device tree for benesse TAB-A05-BD (TAB-A05-BD)
+
+```
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
